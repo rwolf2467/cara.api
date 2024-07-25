8067e61ea0b74aec592a0eff48e330f46b4f40e7
Red_wolf2467 <redwolf2467@pyropixle.com>
2024-07-25 23:47:05 +0200

    d

--- setup.py
+++ setup.py
@@ -5,7 +5,11 @@ with open("readme_pachage.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='cara_api',
+<<<<<<< HEAD
+    version='1.5.2',
+=======
     version='1.5',
+>>>>>>> f1581dd094ade4bed28cfb21d7daab44109c4e54
     packages=["cara_api"],
     install_requires=["requests", "reds-simple-logger"],
     author="Red_Wolf2467",
